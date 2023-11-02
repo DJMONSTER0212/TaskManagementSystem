eaaa9676623abd5972f1b7b51b3f38b8a6191056
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-11-02 22:40:36 +0530

    mark 15

--- app/api/task/index/route.ts
+++ app/api/task/index/route.ts
@@ -1,29 +1,38 @@
 import prismadb from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 import { cookies } from 'next/headers'
+async function getCookieData() {
+    const cookieData = cookies().getAll()
+    return new Promise((resolve) =>
+        setTimeout(() => {
+            resolve(cookieData)
+        }, 1000)
+    )
+}
 
 export async function GET(
-    req:Request,
+    req: Request,
 
-){
-    const cookieData = cookies().getAll()
+) {
+    // const cookieData = cookies().getAll()
+    const cookieData = await getCookieData()
     try {
         // const body = req.json()
-        const {searchParams} = new URL(req.url);
+        const { searchParams } = new URL(req.url);
         const param = searchParams.get("authorId");
 
-        if(!param){
-            return new NextResponse("authorId is Required",{status:400});
+        if (!param) {
+            return new NextResponse("authorId is Required", { status: 400 });
         }
-        const tasks =  await prismadb.task.findMany({
-            where:{
-                authorId : param
+        const tasks = await prismadb.task.findMany({
+            where: {
+                authorId: param
             }
         });
         return NextResponse.json(tasks);
 
     } catch (error) {
-        console.log("[TASK_GET ERROR]",error)
-        return new NextResponse("Internal Error",{status:500});
+        console.log("[TASK_GET ERROR]", error)
+        return new NextResponse("Internal Error", { status: 500 });
     }
 }
